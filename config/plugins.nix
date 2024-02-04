3b8c1e21d72167caaabda863bbf288c76a98c932
Aldo Sierra <aldo.sierrav@gmail.com>
2024-02-04 03:31:08 -0600

    Add lualine, telescope and nvim-tree plugins

--- config/plugins.nix
+++ config/plugins.nix
@@ -9,5 +9,55 @@
 
     # Syntax highlighting
     treesitter.enable = true;
+
+    # Better status line
+    lualine = {
+      enable = true;
+      globalstatus = true;
+      iconsEnabled = true;
+    };
+
+    # File finder
+    telescope = {
+      enable = true;
+      keymaps = {
+        "<leader>ff" = {
+          action = "find_files";
+          desc = "Telescope find files";
+        };
+        "<leader>fg" = {
+          action = "live_grep";
+          desc = "Telescope live grep";
+        };
+        "<leader>fb" = {
+          action = "buffers";
+          desc = "Telescope buffers files";
+        };
+        "<leader>fh" = {
+          action = "help_tags";
+          desc = "Telescope help tags";
+        };
+        "<leader>fp" = {
+          action = "git_files";
+          desc = "Telescope git files";
+        };
+      };
+    };
+
+    # Better file explorer
+    nvim-tree = {
+      enable = true;
+      autoClose = true;
+      actions.openFile.quitOnOpen = true;
+      autoReloadOnWrite = true;
+      openOnSetup = true;
+      hijackNetrw = true;
+      hijackCursor = true;
+      git = {
+        enable = true;
+        ignore = true;
+      };
+    };
   };
 }
+
