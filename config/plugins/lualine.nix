4e8579f848bcce78673979d5014dd5a2679941e9
Aldo Sierra <aldo.sierrav@gmail.com>
2024-05-01 22:29:15 -0600

    Change luoline theme

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -1,7 +1,57 @@
+let
+  colors = {
+    blue   = "#80a0ff";
+    cyan   = "#79dac8";
+    black  = "#080808";
+    white  = "#c6c6c6";
+    red    = "#ff5189";
+    violet = "#d183e8";
+    grey   = "#303030";
+  };
+in
 {
   plugins.lualine = {
     enable = true;
     globalstatus = true;
     iconsEnabled = true;
+    theme = {
+      normal = {
+        a = { fg = colors.black; bg = colors.blue; };
+        b = { fg = colors.white; bg = colors.black; };
+        c = { fg = colors.white; };
+      };
+      insert = { a = { fg = colors.black; bg = colors.cyan; }; };
+      visual = { a = { fg = colors.black; bg = colors.violet; }; };
+      replace = { a = { fg = colors.black; bg = colors.red; }; };
+      inactive = {
+        a = { fg = colors.white; bg = colors.black; };
+        b = { fg = colors.white; bg = colors.black; };
+        c = { fg = colors.white; };
+      };
+    };
+    componentSeparators = {
+      left = "";
+      right = "";
+    };
+    sectionSeparators = {
+      left = "";
+      right = "";
+    };
+    sections = {
+      lualine_a = [ { name = "mode"; separator.left = ""; padding = {right = 2; left = 1; }; } ];
+      lualine_b = [ "filename" "branch" ];
+      lualine_c = [ "%=" ];
+      lualine_x = null;
+      lualine_y = [ "filetype" "progress" ];
+      lualine_z = [ { name = "location"; separator.right = ""; padding = {left = 2; right = 1; }; } ];
+    };
+    inactiveSections = {
+      lualine_a = [ "filename" ];
+      lualine_b = null;
+      lualine_c = null;
+      lualine_x = null;
+      lualine_y = null;
+      lualine_z = [ "location" ];
+    };
   };
 }
