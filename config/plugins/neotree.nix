398df77c514ea49fdef7e574bfe11002ae3404ef
Aldo Sierra <aldo.sierrav@gmail.com>
2024-05-01 22:29:32 -0600

    Add close on open

--- config/plugins/neotree.nix
+++ config/plugins/neotree.nix
@@ -2,5 +2,12 @@
   plugins.neo-tree = {
     enable = true;
     closeIfLastWindow = true;
+    eventHandlers = {
+      file_opened = ''
+        function (file_path)
+          require("neo-tree.command").execute({ action = "close" })
+        end
+      '';
+    };
   };
 }
